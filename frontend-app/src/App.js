ef2279765de0068ffb137990b68fe06941c3763a
rajpootmegha7 <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-12 13:31:17 -0500

    react container files and redirection

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Home from './containers/home/home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/Login" />
+          </Route>
+          <Route path="/Login" exact component={Login} />
+          <Route path="/Register" exact component={Register} />
+          <Route path="/Home" exact component={Home} />
+      
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </div>
   );
 }
 
