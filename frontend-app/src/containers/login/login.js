b5d803ccd04d3a26b244b31227e96a8e717c07a8
Rachel Wang <wangr15@rpi.edu>
2022-11-12 20:20:54 -0500

    initial setup for login frontend
    - draft login component (in progress)
    - starting tailwind setup
    - adding .DS_Store to gitignore

--- frontend-app/src/containers/login/login.js
+++ frontend-app/src/containers/login/login.js
@@ -1,10 +1,56 @@
 // javascript login file for login functionality.
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-export default class login extends Component {
+
+
+class LoginForm extends React.Component {
+  constructor(props){
+    super(props);
+    
+  }
+
   render() {
     return (
-      <h1>login page</h1>
+      <div className="login">
+        <form action=""> {/*attach function here*/}
+          {/* placeholder for potential hero image */}
+          <img src="" alt="" /> 
+        
+          <div id="usernameGroup">
+            <label>Username</label>
+            <input type="username" placeholder="Enter username"  />
+          </div>
+         
+          <div id="emailGroup"> 
+            <label for="email">Email address</label>
+            <input id="email" type="email" placeholder="Enter email address"  />
+          </div>
+          <div id="passwordGroup">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder = "Enter password" />    
+            <a class="sp-underline"href="!putendpointhere">Forgot password?</a>
+          </div>
+          <button class="login-register">Log In</button>
+        </form>
+        <p>Don't have an account?</p><a href="!putendpointhere">Register</a>
+      </div>
     )
   }
 }
+
+export default class Login extends Component {
+  render() {
+    return (
+        <div className="login">
+          <h1> Cyber Attacks Map </h1>
+          <LoginForm />
+        </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Login/>);
