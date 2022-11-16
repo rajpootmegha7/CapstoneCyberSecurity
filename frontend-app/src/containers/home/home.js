45125613bb5172ed663bb38fbc5e47f3be92941e
Rachel Wang <wangr15@rpi.edu>
2022-11-16 11:02:17 -0500

    renaming components
    - Renaming chart to map, adding some css and commenting out example data

--- frontend-app/src/containers/home/home.js
+++ frontend-app/src/containers/home/home.js
@@ -1,9 +1,9 @@
 // Java script file for interactive map on our home page.
-import React, { Component } from 'react'
-import Chart from './chart'
+import React, { Component, useState, useEffect } from 'react'
+import Map from './map'
 import * as d3 from 'd3'; 
 
- const data = [
+/*  const data = [
   {year: 1980, efficiency: 24.3, sales: 8949000},
   {year: 1985, efficiency: 27.6, sales: 10979000},
   {year: 1990, efficiency: 28, sales: 9303000},
@@ -34,7 +34,7 @@ import * as d3 from 'd3';
   {year: 2015, efficiency: 37.2, sales: 7517000},
   {year: 2016, efficiency: 37.7, sales: 6873000},
   {year: 2017, efficiency: 39.4, sales: 6081000},
-]
+] */
 
 /* class MainMap extends React.Component {
  render() {
@@ -61,7 +61,11 @@ function Home() {
 export default class Home extends Component {
    render () {
     return(
-      <Chart data={data} />
+
+      <div>
+        <Map id="map"></Map>
+      </div>
+      
     )
    }
 }
